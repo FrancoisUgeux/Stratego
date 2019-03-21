97ce7112a8a3dbb1001a18b7b3c126c60f7dd254
franc <franc@DESKTOP-THQUH7G.lan>
2019-03-21 18:44:55 +0100

    Commit3 added Square and Position

---
+++ Stratego/src/g43335/stratego/model/Square.java
@@ -0,0 +1,21 @@
+package g43335.stratego.model;
+
+/**
+ *
+ * @author franc
+ */
+public class Square {
+    private Piece piece;
+
+    public Square(Piece piece) {
+        this.piece = piece;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+    
+    public static void put(Piece piece){
+        
+    }
+}
