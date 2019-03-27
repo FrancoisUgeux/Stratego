f3dfc95d17b4a55e6ee4a750c898884991d8dabf
franc <franc@DESKTOP-THQUH7G.lan>
2019-03-27 18:27:37 +0100

    Commit15 worked on view / displayBoard

--- Stratego/src/g43335/stratego/view/View.java
+++ Stratego/src/g43335/stratego/view/View.java
@@ -1,5 +1,6 @@
 package g43335.stratego.view;
 
+import g43335.stratego.model.PlayerColor;
 import g43335.stratego.model.Square;
 import java.util.Scanner;
 
@@ -57,38 +58,25 @@ public class View {
         for (int i = 0; i < squares.length; i++) {
             System.out.print("row#0" + i + " || ");
             for (int j = 0; j < squares[i].length; j++) {
+                String ColorPiece;
+                if (squares[i][j].getPiece().getColor() == PlayerColor.BLUE) {
+                    ColorPiece = ANSI_WHITE_BACKGROUND + ANSI_BLUE
+                            + "PE" + ANSI_RESET;
+                } else {
+                    ColorPiece = ANSI_WHITE_BACKGROUND + ANSI_RED
+                            + "PE" + ANSI_RESET;
+                }
+
                 if (squares[i][j] == null) {
-                    System.out.print(ANSI_WHITE_BACKGROUND + "  " + ANSI_RESET);
-                } else{
+                    System.out.print(" | " + ANSI_WHITE_BACKGROUND 
+                            + "  " + ANSI_RESET + " | ");
+                } else {
                     // getColor => variable || 
                     //getcolor = square[i][j].getcolor 
-                    System.out.print("|" + ANSI_WHITE_BACKGROUND + squares[i][j] + ANSI_RESET);
+                    System.out.print(" | " + ColorPiece + " | ");
                 }
             }
         }
-        /*
-        System.out.println("row#00 || | "
-                + ANSI_BLACK_BACKGROUND + squares[0][0] + ANSI_RESET + " | | "
-                + ANSI_BLACK_BACKGROUND + squares[1][0] + ANSI_RESET + " | | "
-                + ANSI_BLACK_BACKGROUND + squares[2][0] + ANSI_RESET + " | | "
-                + ANSI_BLACK_BACKGROUND + squares[3][0] + ANSI_RESET + " |");
-        System.out.println("row#01 || | "
-                + ANSI_BLACK_BACKGROUND + squares[0][1] + ANSI_RESET + " | | "
-                + ANSI_BLACK_BACKGROUND + squares[1][1] + ANSI_RESET + " | | "
-                + ANSI_BLACK_BACKGROUND + squares[2][1] + ANSI_RESET + " | | "
-                + ANSI_BLACK_BACKGROUND + squares[3][1] + ANSI_RESET + " |");
-        System.out.println("row#02 || | "
-                + ANSI_BLACK_BACKGROUND + squares[0][2] + ANSI_RESET + " | | "
-                + ANSI_BLACK_BACKGROUND + squares[1][2] + ANSI_RESET + " | | "
-                + ANSI_BLACK_BACKGROUND + squares[2][2] + ANSI_RESET + " | | "
-                + ANSI_BLACK_BACKGROUND + squares[3][2] + ANSI_RESET + " |");
-        System.out.println("row#03 || | "
-                + ANSI_BLACK_BACKGROUND + squares[0][3] + ANSI_RESET + " | | "
-                + ANSI_BLACK_BACKGROUND + squares[1][3] + ANSI_RESET + " | | "
-                + ANSI_BLACK_BACKGROUND + squares[2][3] + ANSI_RESET + " | | "
-                + ANSI_BLACK_BACKGROUND + squares[3][3] + ANSI_RESET + " |");
-        System.out.println();
-         */
     }
 
     public void displayOver() {
